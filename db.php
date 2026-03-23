3e186b3252b6ec8b70faeb3f21ce46dfad7b8c5a
shravanhuu-boop <shravanhuu@gmail.com>
2026-03-23 22:46:11 +0530

    Update database connection parameters in db.php

--- db.php
+++ db.php
@@ -1,9 +1,13 @@
 <?php
+$host = "centerbeam.proxy.rlwy.net";
+$user = "root";
+$pass = "PTrPrukWUwbwdMzAgUmVhMscFUsUiNlb";
+$db   = "railway";
+$port = "52817";
 
-$conn = mysqli_connect("localhost","root","","lms");
+$conn = mysqli_connect($host, $user, $pass, $db, $port);
 
-if(!$conn){
+if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
-
 ?>
